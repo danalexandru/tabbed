680f3b4b430e3dbcedddddb22ec0911385f0788c
danalexandru <danb.alexandru@gmail.com>
2022-09-03 19:49:46 +0300

    add: Added colorscheme.h that allows easy changing of tabbed colorschemes

---
+++ colorschemes.h
@@ -0,0 +1,59 @@
+/************************************/
+/**     COLORSCHEME COMPONENTS      */
+/************************************/
+#define SIZE_STRING 8
+
+typedef struct {
+    char Background[SIZE_STRING];
+    char Foreground[SIZE_STRING];
+} Scheme;
+
+typedef struct {
+    Scheme Normal;
+    Scheme Select;
+} ColorScheme;
+
+
+/************************************/
+/**         COLORSCHEMES            */
+/************************************/
+const ColorScheme Default = {
+    .Normal = {
+        .Background = "#222222",
+        .Foreground = "#bbbbbb",
+    },
+    .Select = {
+        .Background = "#005577",
+        .Foreground = "#eeeeee",
+    }
+};
+
+const ColorScheme DoomOne = {
+    .Normal = {
+        .Background = "#14191F",
+        .Foreground = "#8C92AC",
+    },
+    .Select = {
+        .Background = "#1F2833",
+        .Foreground = "#F3F3F6",
+    }
+};
+
+const ColorScheme MatchaDark = {
+    .Normal = {
+        .Background = "#141a1b",
+        .Foreground = "#6c8782",
+        /* .Foreground = "#16a085", */
+    },
+    .Select = {
+        .Background = "#263034",
+        .Foreground = "#F3F3F6",
+    }
+};
+
+
+/************************************/
+/**      CURRENT COLORSCHEME        */
+/************************************/
+const ColorScheme CurrentColorScheme = MatchaDark;
+
